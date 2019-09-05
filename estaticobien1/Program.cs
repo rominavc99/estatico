837fc543c25a71651d7941d25db9b4b193b1f633
rominavc99 <procrom1999@gmail.com>
2019-09-04 20:46:25 -0700

    Agregar archivos de proyecto.

---
+++ estaticobien1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estaticobien1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematicas matematicas = new Matematicas();
+            double sumando1 = 10;
+            double sumando2 = 43;
+            double resultado = Matematicas.sumar(sumando1, sumando2);
+
+            Console.WriteLine("PI:" + Matematicas.PI);
+            Console.WriteLine("PI:" + Matematicas.PI);
+
+            Console.WriteLine(sumando1.ToString() + " + " + sumando2.ToString() + " = " + resultado.ToString());
+            Console.Read();
+
+
+        }
+    }
+}
